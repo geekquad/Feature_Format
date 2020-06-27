be6f954543f9bb7441ad7d2958a47d1c9664f187
Aditya Kumar Gupta <adityaastranaut@gmail.com>
2020-06-28 00:36:32 +0530

    Add files via upload

--- Feature_Format/setup.py
+++ Feature_Format/setup.py
@@ -12,7 +12,7 @@ setup(
   keywords = ['Feature_Format', 'Target_Split'],   # Keywords that define your package best
   install_requires=['numpy'],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
